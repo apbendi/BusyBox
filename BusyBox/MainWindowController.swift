51583e5c67fd395d342774fea08462e4da687ebd
Ben DiFrancesco <ben.difrancesco@gmail.com>
2015-05-11 23:04:01 -0400

    Add Slider

--- BusyBox/MainWindowController.swift
+++ BusyBox/MainWindowController.swift
@@ -2,8 +2,20 @@ import Cocoa
 
 class MainWindowController : NSWindowController {
 
+    @IBOutlet weak var sliderStatusLabel: NSTextField!
+    @IBOutlet weak var slider: NSSlider!
+
+    var lastSliderValue = 0.0
+
     override var windowNibName: String? {
         return "MainWindowController"
+    }
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        lastSliderValue = slider.doubleValue
+        sliderStatusLabel.stringValue = ""
     }
 
     @IBAction func boxDidCheck(sender: NSButton) {
@@ -12,6 +24,18 @@ class MainWindowController : NSWindowController {
         } else {
             sender.title = "Check Me"
         }
+    }
+
+    @IBAction func sliderDidUpdate(sender: NSSlider) {
+        if sender.doubleValue > lastSliderValue {
+            sliderStatusLabel.stringValue = "Slider Goes Up!"
+        } else if sender.doubleValue < lastSliderValue {
+            sliderStatusLabel.stringValue = "Slider Goes Down!"
+        } else {
+            sliderStatusLabel.stringValue = "Slider Stays the same!"
+        }
+
+        lastSliderValue = sender.doubleValue
     }
 
 }
