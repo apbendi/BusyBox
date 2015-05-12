30e500ee085642ff864516c313c553140f13a373
Ben DiFrancesco <ben.difrancesco@gmail.com>
2015-05-11 22:50:47 -0400

    Add checkbox

--- BusyBox/MainWindowController.swift
+++ BusyBox/MainWindowController.swift
@@ -5,4 +5,13 @@ class MainWindowController : NSWindowController {
     override var windowNibName: String? {
         return "MainWindowController"
     }
+
+    @IBAction func boxDidCheck(sender: NSButton) {
+        if sender.state == NSOnState {
+            sender.title = "Uncheck me"
+        } else {
+            sender.title = "Check Me"
+        }
+    }
+
 }
